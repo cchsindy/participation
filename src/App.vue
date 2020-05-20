959dbd74f4714d6548aa1e796eaa4c33f68c9f7d
Brad Spencer <bradspencer@Brads-Mac-mini.attlocal.net>
2020-05-20 13:22:59 -0400

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Athletic Participation</h1>
+    <h2>Step 1</h2>
+    <p>
+      Download and print the
+      <a
+        href="https://ihsaa.org/Portals/0/ihsaa/documents/quick%20resources/2020-21%20Physical%20Form.pdf?ver=2020-01-17-154323-640"
+        target="_blank"
+      >IHSAA Physical Form</a>,
+      complete all 5 pages, and scan or take a picture of each page to upload below.
+    </p>
+    <p>
+      <i>All images must be in JPEG format (.jpg) and less than 4MB each.</i>
+    </p>
+    <p>
+      Upload IHSAA form page 1
+      <br />
+      <input type="file" />
+    </p>
+    <p>
+      Upload IHSAA form page 2
+      <br />
+      <input type="file" />
+    </p>
+    <p>
+      Upload IHSAA form page 3
+      <br />
+      <input type="file" />
+    </p>
+    <p>
+      Upload IHSAA form page 4
+      <br />
+      <input type="file" />
+    </p>
+    <p>
+      Upload IHSAA form page 5
+      <br />
+      <input type="file" />
+    </p>
+    <button>Step 2</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
