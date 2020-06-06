62e28fede526e2abde52764fc14971a97c2cafda
Brad Spencer <bradspencer@Brads-Mac-mini.attlocal.net>
2020-06-06 12:35:18 -0400

    added firebase

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div id="app">
     <h1>Athletic Participation</h1>
-    <component :is="currentStep" />
+    <component ref="comp" :is="currentStep" @saveStep="saveStep" />
     <button v-if="currentIndex < 6" @click="nextStep">Next Step</button>
   </div>
 </template>
 
 <script>
-import Step0 from "@/components/Step0";
-import Step1 from "@/components/Step1";
-import Step2 from "@/components/Step2";
-import Step3 from "@/components/Step3";
-import Step4 from "@/components/Step4";
-import Step5 from "@/components/Step5";
-import Step6 from "@/components/Step6";
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import Step0 from '@/components/Step0'
+import Step1 from '@/components/Step1'
+import Step2 from '@/components/Step2'
+import Step3 from '@/components/Step3'
+import Step4 from '@/components/Step4'
+import Step5 from '@/components/Step5'
+import Step6 from '@/components/Step6'
+
+const app = firebase.initializeApp({
+  apiKey: 'AIzaSyCg_FVHdzP3kvRAyrnpE2bJUsQfMRUgFW4',
+  authDomain: 'my-covenant.firebaseapp.com',
+  databaseURL: 'https://my-covenant.firebaseio.com',
+  projectId: 'my-covenant',
+  storageBucket: 'my-covenant.appspot.com',
+  messagingSenderId: '945207168321',
+  appId: '1:945207168321:web:e42d0845df84c8c24e65c0',
+})
+// eslint-disable-next-line no-unused-vars
+const store = app.firestore()
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     Step0,
     Step1,
@@ -24,21 +38,44 @@ export default {
     Step3,
     Step4,
     Step5,
-    Step6
+    Step6,
   },
   data: () => {
     return {
       currentIndex: 0,
-      currentStep: "Step0"
-    };
+      currentStep: 'Step0',
+      docID: null,
+    }
   },
   methods: {
+    advanceStep() {
+      this.currentIndex++
+      this.currentStep = 'Step' + this.currentIndex
+    },
     nextStep() {
-      this.currentIndex++;
-      this.currentStep = "Step" + this.currentIndex;
-    }
-  }
-};
+      if (this.currentIndex === 0) {
+        const student = this.$refs.comp.$refs.studentName.value.trim()
+        const parent = this.$refs.comp.$refs.parentName.value.trim()
+        store
+          .collection('athletic_participation')
+          .add({
+            started: firebase.firestore.Timestamp.fromDate(new Date()),
+            student,
+            parent,
+          })
+          .then((result) => {
+            if (result.id) {
+              this.docID = result.id
+              this.advanceStep()
+            }
+          })
+      }
+    },
+    saveStep(data) {
+      console.log(data)
+    },
+  },
+}
 </script>
 
 <style>
--- src/components/Step0.vue
+++ src/components/Step0.vue
@@ -4,7 +4,9 @@
     <ul>
       <li>
         Login to
-        <a href="https://covenantchristian.myschoolapp.com" target="_blank">MySchoolApp</a>
+        <a href="https://covenantchristian.myschoolapp.com" target="_blank"
+          >MySchoolApp</a
+        >
         and confirm that you have completed the Student Emergency Contacts,
         Student Medical Information, and Non-prescription Medication Permission
         forms.
@@ -14,27 +16,33 @@
         <a
           href="https://ihsaa.org/Portals/0/ihsaa/documents/quick%20resources/2020-21%20Physical%20Form.pdf?ver=2020-01-17-154323-640"
           target="_blank"
-        >IHSAA Physical Form</a>, complete all 5 pages, and scan or take a photo of each page to upload
-        in Step 2. Images must be in JPEG format (.jpg) and less than 4MB each. NOTE: 2019-2020 Covenant students who completed the 2019-2020 IHSAA Form do not need to complete the 2020-2021 IHSAA Form.
+          >IHSAA Physical Form</a
+        >, complete all 5 pages, and scan or take a photo of each page to upload
+        in Step 2. Images must be in JPEG format (.jpg) and less than 4MB each.
+        NOTE: 2019-2020 Covenant students who completed the 2019-2020 IHSAA Form
+        do not need to complete the 2020-2021 IHSAA Form.
       </li>
       <li>
         Parents and student athletes must read our
         <a
           href="https://docs.google.com/document/d/1VpeB8edyIshu_O84cumb7E3S_UeaY6FFgULrsACuqzs"
           target="_blank"
-        >Athletic Handbook</a>.
+          >Athletic Handbook</a
+        >.
       </li>
       <li>
         Parents must read the
         <a
           href="https://www.cdc.gov/headsup/pdfs/highschoolsports/parents_fact_sheet-a.pdf"
           target="_blank"
-        >CDC Heads Up</a>
+          >CDC Heads Up</a
+        >
         and
         <a
           href="http://www.doe.in.gov/sites/default/files/health/sudden-cardiac-arrest-fact-sheet-parents.pdf"
           target="_blank"
-        >DOE Sudden Cardiac Arrest</a>
+          >DOE Sudden Cardiac Arrest</a
+        >
         fact sheets.
       </li>
       <li>
@@ -42,23 +50,25 @@
         <a
           href="https://www.cdc.gov/headsup/pdfs/highschoolsports/athletes_fact_sheet-508.pdf"
           target="_blank"
-        >CDC Heads Up</a>
+          >CDC Heads Up</a
+        >
         and
         <a
           href="http://www.doe.in.gov/sites/default/files/health/sudden-cardiac-arrest-fact-sheet-student-athletes.pdf"
           target="_blank"
-        >DOE Sudden Cardiac Arrest</a>
+          >DOE Sudden Cardiac Arrest</a
+        >
         fact sheets.
       </li>
     </ul>
     <h3>Your Information</h3>
     <p>
       Student/Athlete name:
-      <input type="text" />
+      <input ref="studentName" type="text" />
     </p>
     <p>
       Parent/Guardian name:
-      <input type="text" />
+      <input ref="parentName" type="text" />
     </p>
     <p>Steps 3 and 4 require your student's participation.</p>
     <p>
@@ -69,7 +79,7 @@
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style scoped></style>
